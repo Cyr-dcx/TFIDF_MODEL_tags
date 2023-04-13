5add4774ded8f3999d7a6cd7b264b69ee58e1219
Cyr-Dcx <cyrielle.deceneux@gmail.com>
2023-04-13 11:00:11 +0200

    test sans fonciton

--- main.py
+++ main.py
@@ -5,34 +5,36 @@
 import joblib
 from fastapi import FastAPI #HTTPException
 from pydantic import BaseModel
+import sys
 
 # pour le preprocessing de la question
 from utils_package.functions import final_cleaning
 
-encoder_file = "./target_encoder.sav"
-tfidf_file = "./tfidf_encoder.sav"
-model_file = "./tfidf_lightGBM.sav"
+# encoder_file = "./target_encoder.sav"
+# tfidf_file = "./tfidf_encoder.sav"
+# model_file = "./tfidf_lightGBM.sav"
 
-target_encoder = joblib.load(encoder_file)
-tfidf = joblib.load(tfidf_file)
-model = joblib.load(model_file)
+# target_encoder = joblib.load(encoder_file)
+# tfidf = joblib.load(tfidf_file)
+# model = joblib.load(model_file)
 
-def preprocess_pipeline(question):
-    question_list = []
-    preprocessed_question = final_cleaning(question, token=False)
-    question_list.append(str(preprocessed_question))
-    X_tfidf = tfidf.transform(question_list)
-    return X_tfidf
 
-def generate_prediction(preprocessed_question, my_model=model):
-    tags = my_model.predict(preprocessed_question)
-    return tags
+# def preprocess_pipeline(question):
+#     question_list = []
+#     preprocessed_question = final_cleaning(question, token=False)
+#     question_list.append(str(preprocessed_question))
+#     X_tfidf = tfidf.transform(question_list)
+#     return X_tfidf
 
-def predict_pipeline(question,target_encoder=target_encoder):
-    preprocessed_question = preprocess_pipeline(question)
-    predictions = generate_prediction(preprocessed_question)
-    tags = target_encoder.inverse_transform(predictions)
-    return tags
+# def generate_prediction(preprocessed_question, my_model=model):
+#     tags = my_model.predict(preprocessed_question)
+#     return tags
+
+# def predict_pipeline(question,target_encoder=target_encoder):
+#     preprocessed_question = preprocess_pipeline(question)
+#     predictions = generate_prediction(preprocessed_question)
+#     tags = target_encoder.inverse_transform(predictions)
+#     return tags
     
 app = FastAPI()
 
@@ -48,11 +50,11 @@ sentence_test="I've been making Python scripts for simple tasks at work and neve
 def index():
     return {"tags": "Faisons une prédiction"}
 
-@app.post("/predict", status_code=200)
-def read_item(one_phrase: Phrase):
-    question = one_phrase.phrase
-    preprocessed_question = preprocess_pipeline(question)
-    predictions = generate_prediction(preprocessed_question)
-    tags = target_encoder.inverse_transform(predictions)
+# @app.post("/predict", status_code=200)
+# def read_item(one_phrase: Phrase):
+#     question = one_phrase.phrase
+#     preprocessed_question = preprocess_pipeline(question)
+#     predictions = generate_prediction(preprocessed_question)
+#     tags = target_encoder.inverse_transform(predictions)
 
-    return {"tags": tags}
+#     return {"tags": tags}
